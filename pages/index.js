4edd652f481453325a21466d0b03e06ebd052199
cocacola-lover <pegas620570@gmail.com>
2023-03-06 22:54:59 +0300

    Extracted FormForClient

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Container from "../components/Container";
 import ForClient from "../components/ForClient"
 import Footer from "../components/Footer"
 import TextInputFormComponent, {InputFieldType} from "../components/TextInputFormComponent";
+import FormForClient from "../components/FormForClient";
 
 export default function Home() {
   return (
@@ -25,62 +26,7 @@ export default function Home() {
         <ForClient />
         <Footer />
       
-      <form style={{width : '100%', height: '50vh', display : 'flex', flexDirection : 'column'}}>
-        <div style={{width : '100%', flex : 1}}>
-          <TextInputFormComponent
-            label={'Имя юридического/физического лица'}
-            id={'Employer_name'}
-            placeholder={'Иванов Иван Владимирович'}
-            type={InputFieldType.Text}
-            required
-          />
-        </div>
-        <div style={{width : '100%', flex : 1}}>
-          <TextInputFormComponent
-            label={'Почта для связи'}
-            id={'Employer_email'}
-            placeholder={'example@supermail.com'}
-            type={InputFieldType.Email}
-            required
-          />
-        </div>
-        <div style={{width : '100%', flex : 1}}>
-          <TextInputFormComponent
-            label={'Номер телефона'}
-            id={'Employer_phone'}
-            placeholder={'81234567890'}
-            type={InputFieldType.PhoneNumber}
-            required
-          />
-        </div>
-        <div style={{width : '100%', flex : 2}}>
-          <TextInputFormComponent
-            label={'Краткая идея проекта'}
-            id={'Employer_idea'}
-            placeholder={'Хочу летающие машины'}
-            type={InputFieldType.Area}
-            required
-          />
-        </div>
-        <div style={{width : '100%', flex : 4}}>
-          <TextInputFormComponent
-            label={'Описание проекта'}
-            id={'Employer_project_description'}
-            placeholder={'Хочу прям такие крутящиеся летающие машины, которые пи-пи-пиу'}
-            type={InputFieldType.Area}
-            required vertical
-          />
-        </div>
-        <div style={{width : '100%', flex : 1}}>
-          <TextInputFormComponent
-            label={'Другие способы связи'}
-            id={'Employer_contacts'}
-            placeholder={'Мой второй телефон, моя мама...'}
-            type={InputFieldType.Text}
-          />
-        </div>
-        <button type="submit">submit</button>
-      </form>
+      <FormForClient styles={{width : '100%', height: '80vh'}}/>
     </div>
   );
 }
