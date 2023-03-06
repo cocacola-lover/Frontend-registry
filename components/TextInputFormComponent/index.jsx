39b7eb1cf886aebddfd5ff6798d64414846496aa
cocacola-lover <pegas620570@gmail.com>
2023-03-06 22:54:01 +0300

    Extracted components and added description

--- components/TextInputFormComponent/index.jsx
+++ components/TextInputFormComponent/index.jsx
@@ -1,17 +1,25 @@
 import styles from "./TextInputFormComponent.module.scss";
 
-import { useEffect, useRef } from "react";
-
+import { TextInputField, NumberInputField, EmailInputField, AreaInputField } from "./FieldComponents";
 /*
-    TextInputField props :
-    requiered : 
+    This component was build to be reusable if needed. So please consider using if it fits in your component.
+    Keep in mind most of validation happens on submit event only.
+
+    TextInputFormComponent props :
+    required : 
         id : string - Every input shoud have a unique id,
         label : string - Every input shoud have a label
         type : InputFieldType - Type of input
     optional : 
         placeholder : string
-        required : boolean - puts star after label
+        required : boolean
         vertical : boolean
+
+        ratio : number - describes ration of space between input and label
+        minLength : number
+        maxLength : number
+
+        name : string - for submitting form
 */
 
 
@@ -23,116 +31,22 @@ export const InputFieldType = {
     PhoneNumber : 4
 }
 
-function BasicInputField (basicInputProps) {
-
-    const props = basicInputProps;
-
-    const inputRef = useRef(null);
-
-    // Handle Validation
-    useEffect(() => {
-        const input = inputRef.current;
-
-        if (input === null) return;
-
-        function handleValidation () {
-
-            input.setCustomValidity('');
-
-            if (!input.validity.valid) {
-                if (props.required && input.validity.valueMissing) {
-                    input.setCustomValidity('This field is required');
-                }
-                else if (input.validity.tooLong) {
-                    input.setCustomValidity(`This should not be longer than ${props.maxLength} symbols`);
-                }
-                else if (input.validity.tooShort) {
-                    input.setCustomValidity(`This should not be shorter than ${props.minLength} symbols`);
-                }
-                props.additionalValidation && props.additionalValidation(input);
-            }
-        }
-
-        input.addEventListener('input', handleValidation);
-
-        return () => {
-            input.removeEventListener('input', handleValidation);
-        }
-    })
-
-    return (
-        <input className={`${styles.Input}`}
-        pattern={props.pattern}
-        ref={inputRef}
-        type={props.type} 
-        id={props.id} 
-        name={props.name ?? props.id}
-        style={{flex : props.ratio}}
-        placeholder={props.placeholder}
-        required={props.required}
-        minLength={props.minLength}
-        maxLength={props.maxLength}
-        />
-    )
-}
-
-function TextInputField (TextInputProps) {
-    
-    const props = TextInputProps;
-
-    return (<BasicInputField type='text' id={props.id} ratio={props.ratio} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>)
-}
-
-function EmailInputField (EmailInputProps) {
-
-    const props = EmailInputProps;
-
-    function additionalValidation (input) {
-        if (input.validity.typeMismatch) {
-            input.setCustomValidity('Please put a valid email address');
-        }
-    }
-
-    return (<BasicInputField type='email' additionalValidation={additionalValidation} id={props.id} ratio={props.ratio} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>)
-}
-
-function NumberInputField (NumberInputProps) {
-
-    const props = NumberInputProps;
-
-    function additionalValidation (input) {
-        if (input.validity.patternMismatch) {
-            input.setCustomValidity('Please put a valid phone number');
-        }
-    }
-
-    return (<BasicInputField pattern={'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$'}
-        type='tel' additionalValidation={additionalValidation} id={props.id} ratio={props.ratio} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>)
-
-}
-
-function AreaInputField (AreaInputProps) {
-    const props = AreaInputProps;
-
-    return <textarea id={props.id} className={`${styles.Input} ${styles.Area}`} style={{flex : props.ratio, resize: 'none'}} name={props.name} required={props.required} placeholder={props.placeholder} maxLength={props.maxLength}/>
-}
-
 export default function TextInputFormComponent (props) {
 
     function buildByType (type) {
         switch (type) {
 
             case InputFieldType.Text :
-                return <TextInputField id={props.id} ratio={1} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>;
+                return <TextInputField id={props.id} ratio={props.ratio ?? 1} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>;
 
             case InputFieldType.Email :
-                return <EmailInputField id={props.id} ratio={1} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>;
+                return <EmailInputField id={props.id} ratio={props.ratio ?? 1} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>;
 
             case InputFieldType.PhoneNumber :
-                return <NumberInputField id={props.id} ratio={1} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>
+                return <NumberInputField id={props.id} ratio={props.ratio ?? 1} placeholder={props.placeholder} required={props.required} minLength={props.minLength} maxLength={props.maxLength} name={props.name}/>
 
             case InputFieldType.Area : 
-                return <AreaInputField id={props.id} ratio={1} placeholder={props.placeholder} required={props.required} maxLength={props.maxLength} name={props.name} />
+                return <AreaInputField id={props.id} ratio={props.ratio ?? 1} placeholder={props.placeholder} required={props.required} maxLength={props.maxLength} name={props.name} />
         }
     }
 
